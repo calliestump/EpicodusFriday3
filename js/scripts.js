d89bec08e9c5291e2ebc5be544628ddb2de40187
Callie Stump <callie@stu.mp>
2020-10-30 12:06:45 -0400

    allows two arrays to compare index and return value based on user input

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("form#roboger").submit(function(event) {
+    let userNum = parseInt($("input#userNumber").val());
+      let numbers = [3,2,1];
+      word = ["Wont you be my neighbor?", "Boop", "Beep"];
+      result = "";
+      for(let i=0; i<numbers.length; i++) {
+        while(userNum>=numbers[i]){
+          result = result + word[i];
+          userNum = userNum - numbers[i]
+        }
+      };
+    $("p").text(result);
+    event.preventDefault();
+  });
+});
